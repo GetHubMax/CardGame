adf41d4945caa6b542a97690a27be72aa4ea104f
GetHubMax <max.harry@hotmail.com>
2017-01-18 19:09:18 +1300

    scirpt

---
+++ Assets/Collection/EditDeckBtn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditDeckBtn : MonoBehaviour {
+	int id=0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void EditDeck(){
+		GameObject.Find ("CollectionMaster").SendMessage ("EditDeck", id);
+
+	}
+
+	public void SetId(int id){
+		this.id = id;
+	}
+
+
+}
